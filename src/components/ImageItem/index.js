76cddf5eed0b927db1cd49516a701bf4ebb88d88
Ömer Hodo <omerhodo@gmail.com>
2023-03-12 14:51:42 +0300

    added some style

--- src/components/ImageItem/index.js
+++ src/components/ImageItem/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import './style.scss'
 
 const ImageItem = ({ image }) => {
   return (
-    <img src={image.urls.small} alt={image.alt_description} />
+    <div className='imageItem'>
+      <img className='imageItem--item' src={image.urls.small} alt={image.alt_description} />
+      <hr />
+      <span className='imageItem--title'>{ image.alt_description }</span>
+    </div>
   )
 }
 
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import './search.scss'
+import './style.scss'
 
 const Search = ({ onSearch }) => {
   const [value, setValue] = useState('');
